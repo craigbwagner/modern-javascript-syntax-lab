e81125dd6b847efc46097430fdcd05c3ea2b990d
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-06-20 10:42:20 -0400

    completed exercise 1

---
+++ lab-file.js
@@ -0,0 +1,12 @@
+//Exercise 1
+// Use `.map()` to iterate over the following array:
+
+const nums = [13, 87, 2, 89, 12, 4, 90, 63];
+
+// Create a new array where each value is multiplied by 2 and log the new array.
+// Your code here
+const multipliedNums = nums.map((n) => {
+	return n * 2;
+});
+
+console.log(multipliedNums);
