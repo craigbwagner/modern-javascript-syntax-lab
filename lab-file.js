0b30de083e6369e5f0ae74c56ca9d782a7990294
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-06-20 13:24:45 -0400

    complete exercise 8

--- lab-file.js
+++ lab-file.js
@@ -71,4 +71,18 @@ userProfile[propertyName] = 'pastafartian'
 
 console.log(userProfile);
 
-//Exercise 7
+//Exercise 8
+// Create a function that takes two parameters, `noun` and `adjective`, both with the following respective default values:
+
+// 1. `cat`
+
+// 2. `white`
+
+// The function should log a sentence 'The cat is white.' by default. The function should substitute the appropriate parameters when supplied arguments.
+
+// Your code here
+function adLib(noun = 'cat', adjective = 'white') {
+    console.log(`The ${noun} is ${adjective}.`);
+}
+
+adLib();
